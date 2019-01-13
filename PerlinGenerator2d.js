7e6c59c3df35731cbc41b200fcd7ee2c74e667ee
Will McVay <mcvay.will@gmail.com>
2019-01-13 11:13:31 -0500

    All teh comments.

--- PerlinGenerator2d.js
+++ PerlinGenerator2d.js
@@ -1,29 +1,93 @@
 export default function PerlinGenerator2d(size, skewed) {
+	// Range of potential results, defined by math and stuff...
 	const RANGE = Math.sqrt(2/4);
-	const MAX_SCALE = size;
+
+	// Rate at which frequency changes between octaves.  Doesn't work very well near as I can tell.
 	const LACUNARITY = 1;
+	// Rate at which amplitude changes between octaves.  Also doesn't seem to work very well.
 	const PERSISTANCE = 1;
 
+	// Perlin noise relies on a grid of unit vectors.  Points between the vectors interpolate values between them.
+	// These vectors have to be initialized to a certain grid size.
 	var gradients = initNoise(size);
 
+	// Initializes the grid of unit vectors.
 	function initNoise(size) {
+		// Start with an empty grid.
 		var grid = [];
+		// Loop through the rows.
 		for (var y=0; y<size+1; y++) {
+			// Start with an empty row.
 			var row = [];
+			// Loop through the columns.
 			for (var x=0; x<size+1; x++) {
+				// Set a random X value for the vector and randomize its direction.
 				var vX = Math.random()*(Math.random()>0.5?1:-1);
+				// Set a random Y value for the vector and randomize its direction.
 				var vY= Math.random()*(Math.random()>0.5?1:-1);
+				// Create a vector using the values and normalize it to a unit vector.
 				var gridVector = new Vector2(vX, vY).normalize();
 
+				// Push the vector onto the end of the row.
 				row.push(gridVector);
 			}
 
+			// After the row is completed, push the whole row onto the end of the grid.
 			grid.push(row);
 		}
 
+		// After all the rows are completed, return the grid.
 		return grid;
 	}
 
+	// Get the value at the specified point.  The scale determines how much of the grid to use.
+	this.getValue = function(scale, x, y) {
+		// Find grid index at scale and identify the four corners.  % size ensures we don't run off the edge of the grid.
+		var x0 = Math.floor(x/scale)%size;
+		var x1 = x0 + 1;
+		var y0 = Math.floor(y/scale)%size;
+		var y1 = y0 + 1;
+
+		// Find the relative position in the grid for the point.
+		var sx = (x-x0*scale)/scale;
+		var sy = (y-y0*scale)/scale;
+
+		// Create vectors from the point to the grid corners.
+		var vX0 = new Vector2(sx, sy);
+		var vX1 = new Vector2(-(1-sx), sy);
+		var vY0 = new Vector2(sx, -(1-sy));
+		var vY1 = new Vector2(-(1-sx), -(1-sy));
+
+		// Compute the dot-products of point->grid corner vectors with the gradient vectors at that corner.
+		// Break execution if we're about to explode.
+		if (typeof gradients[x0] === 'undefined' || typeof gradients[x0][y0] === 'undefined') {debugger;}
+		var n0 = gradients[x0][y0].dotProd(vX0);
+		var n1 = gradients[x1][y0].dotProd(vX1);
+		var n2 = gradients[x0][y1].dotProd(vY0);
+		var n3 = gradients[x1][y1].dotProd(vY1);
+
+		// Interpolate the value using smoothstep.  Linear interpolation makes the grid too easy to see
+		var ix0 = smoothstep(n0, n1, sx);
+		var ix1 = smoothstep(n2, n3, sx);
+		var value = smoothstep(ix0, ix1, sy);
+
+		// Adjust the values to 0.0-1.0 from their usual range.
+		return normalizeValue(value);
+
+		// I plagerized this entirely from stackoverflow.
+		function smoothstep(a0, a1, w) {
+			var value = w*w*w*(w*(w*6 - 15) + 10);
+			return a0 + value*(a1 - a0);
+		}
+
+		// (n-min1)/(max1-min1) maps n from min1-max1 to 0-1.  This can be simplifed for +/- sqrt(2/4).
+		function normalizeValue(value) {
+			return (value + RANGE)/(2*RANGE);
+		}
+	}
+
+	// Plagerized from a YouTube video.  Instead of getting the noise value at the point, get the noise value of the point
+	// scaled to the noise value of the point.  Not entirely sure why it looks cool, but it does.
 	this.getSkewedValue = function(scale, x, y) {
 		x *= this.getValue(scale, x, y);
 		y *= this.getValue(scale, x, y);
@@ -31,9 +95,10 @@ export default function PerlinGenerator2d(size, skewed) {
 		return this.getValue(scale, x, y);
 	}
 
+	// This is a way of doing octaves that sets the persistance and lacunarity fractally by just
+	// dividing the size of the grid sampled and the amplitude by 2 each time.
 	this.getFractalValue = function(scale, x, y) {
 		var result = 0;
-		var baseLog = Math.log(scale)/Math.log(2);
 		var frequency = 1;
 		var amplitude = scale*2;
 		for (var fractal=scale; fractal>1; fractal/=2) {
@@ -46,53 +111,10 @@ export default function PerlinGenerator2d(size, skewed) {
 		return result;
 	}
 
-	this.getValue = function(scale, x, y) {
-		if (typeof scale === 'undefined') {
-			scale = MAX_SCALE;
-		}
-
-		// Find Grid Index at Scale
-		var x0 = Math.floor(x/scale)%size;
-		var x1 = x0 + 1;
-		var y0 = Math.floor(y/scale)%size;
-		var y1 = y0 + 1;
-
-		// Find Relative Position in Grid
-		var sx = (x-x0*scale)/scale;
-		var sy = (y-y0*scale)/scale;
-
-		// Create Vectors from Point to Grid Corners
-		var vX0 = new Vector2(sx, sy);
-		var vX1 = new Vector2(-(1-sx), sy);
-		var vY0 = new Vector2(sx, -(1-sy));
-		var vY1 = new Vector2(-(1-sx), -(1-sy));
-
-		// Compute Dot Products of Point-Grid Vectors to Gradient Vectors
-		if (typeof gradients[x0] === 'undefined' || typeof gradients[x0][y0] === 'undefined') {debugger;}
-		var n0 = gradients[x0][y0].dotProd(vX0);
-		var n1 = gradients[x1][y0].dotProd(vX1);
-		var n2 = gradients[x0][y1].dotProd(vY0);
-		var n3 = gradients[x1][y1].dotProd(vY1);
-
-		// Interpolate Value
-		var ix0 = smoothstep(n0, n1, sx);
-		var ix1 = smoothstep(n2, n3, sx);
-		var value = smoothstep(ix0, ix1, sy);
-
-		return normalizeValue(value);
-
-		function smoothstep(a0, a1, w) {
-			var value = w*w*w*(w*(w*6 - 15) + 10);
-			return a0 + value*(a1 - a0);
-		}
-
-		function normalizeValue(value) {
-			return (value + RANGE)/(2*RANGE);
-		}
-	}
-
+	// This lets us easily switch between the skewed and normal noise generation.
 	this.getAdjustedValue = skewed?this.getSkewedValue:this.getValue;
 
+	// Simple helper class for 2d vectors
 	function Vector2(vX, vY) {
 		var x = vX, y = vY;
 
--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 // Perlin Heat Map POC
 
+// Import the noise module.
 import PerlinGenerator2d from './PerlinGenerator2d.js';
 
+// Set the width and height of the map.
 const MAP_WIDTH = 256;
 const MAP_HEIGHT = 256;
 
+// Set the tile size
 const TILE_SIZE = 16;
 
+// Set the color for 1.0 noise values.
 const COLORS = {
 	red: 0x00,
 	green: 0xF0,
 	blue: 0xDF
 };
 
+
+// Anonymouse function to create a canvas and draw the heatmap on it.
 (function() {
 	var canvasElement = document.createElement('canvas');
 	canvasElement.width = MAP_WIDTH*TILE_SIZE;
@@ -23,55 +29,84 @@ const COLORS = {
 
 	var heatMap = new HeatMap(MAP_WIDTH, MAP_HEIGHT);
 
-	heatMap.draw(ctx, true);
+	heatMap.draw(ctx);
 })();
 
 
 function HeatMap(width, height) {
+	// Anything smaller breaks.  Anything bigger just extends computation time.
+	// TODO: Figure out why.
 	const GRID_SIZE = 128;
 
+	// Instantiate a skewed noise generator
 	var perlin = new PerlinGenerator2d(GRID_SIZE, true);
+	// Sub in the fractal octaves using the grid size
 	var genAlgorithm = perlin.getFractalValue.bind(perlin, GRID_SIZE);
 
+	// Store the max and min.  Our returned values in practice end up
+	// being from about 0.3-0.7, so we want to stretch those out to 0.0-1.0
 	var max, min;
 
+	// Create an empty heat map.
 	var heatMap = [];
 
+	// Export the draw method so it can be drawn to a canvas context.
 	this.draw = draw;
 
+	// Generate the values ahead of time, normalize them to 0.0-1.0 range.
 	generateValues(true);
 
+	// Generates the values and potentially normalizes them.
 	function generateValues(normalize) {
+		// each row
 		for (var y=0; y<height; y++) {
+			// empty row
 			var row = [];
+			// each column
 			for (var x=0; x<width; x++) {
+				// Generate a value for the point, adjust the min/max as needed, push the value onto the row.
 				row.push(stats(genAlgorithm(x,y)));
 			}
+			// Push the row onto the map.
 			heatMap.push(row);
 		}
 
+		// Normalize the values if requested.
 		if (normalize) normalizeValues(this);
 	}
 
+	// Loop through all the values after min/max have been set
 	function normalizeValues() {
+		// each row
 		for (var y=0; y<height; y++) {
+			// each column
 			for (var x=0; x<width; x++) {
+				// Adjust the value based on how far it is from the minimum as a percentage of the range.
 				heatMap[x][y] = ((heatMap[x][y]-min)/(max-min));
 			}
 		}
 	}
 
+	// Stores minimum and maximum return values.
 	function stats(heat) {
+		// If max hasn't been set yet or if the value is bigger, use the current value.
 		if (typeof max === 'undefined' || heat > max) max = heat;
+		// If min hasn't been set yet or if the value is smaller, use the current value.
 		if (typeof min === 'undefined' || heat < min) min = heat;
 
+		// Return the heat value again so we can daisy chain function calls.
 		return heat;
 	}
 
-	function draw(ctx, animate) {
+	// Draws the heatmap onto the canvas context.
+	function draw(ctx) {
+		// Each row
 		for (var y=0; y<height; y++) {
+			// Each column
 			for (var x=0; x<width; x++) {
+				// Set the fill style to the color computed using the returned value and the 1.0 color defined above.
 				ctx.fillStyle = `rgba(${heatMap[x][y]*COLORS.red}, ${heatMap[x][y]*COLORS.green}, ${heatMap[x][y]*COLORS.blue}, 1)`;
+				// Draw a tile at the current location using the size.
 				ctx.fillRect(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE);
 			}
 		}
